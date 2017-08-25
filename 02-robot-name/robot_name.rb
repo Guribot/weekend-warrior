68cc95b12e375c309dae0cc0c97d03fdc5e18ef0
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-08-25 14:14:59 -0700

    complete weekend warrior 02

--- 02-robot-name/robot_name.rb
+++ 02-robot-name/robot_name.rb
@@ -1,1 +1,20 @@
+class Robot
+  attr_reader :name
+  LETTERS = "ABCDEFGHIJKLMNOPQRSTUVWXYZ".chars
+  NUMBERS = "1234567890".chars
 
+  def initialize
+    build_name
+  end
+
+  def build_name
+    name_build = []
+    2.times {name_build << LETTERS.sample}
+    3.times {name_build << NUMBERS.sample}
+    @name = name_build.join
+  end
+
+  def reset
+    build_name
+  end
+end
