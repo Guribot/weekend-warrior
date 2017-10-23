0f1676321596155308e7434244adcbbc864892b4
Kate Evans-Spitzer <kateevans1014@gmail.com>
2017-10-23 09:46:38 -0700

    add rakefile, implement Alouette class

--- 05-alouette/alouette.rb
+++ 05-alouette/alouette.rb
@@ -1,10 +1,24 @@
 class Alouette
+  @refrain = "Alouette, gentille alouette,\nAlouette, je te plumerai."
+  @end = "\nAlouette!\nAlouette!\nA-a-a-ah"
+  @lines = ["la tête", "le bec", "les yeux", "le cou", "les ailes", "les pattes", "la queue", "le dos"]
+
   def self.lines_for_verse(verse_num)
+    return @lines[0..verse_num].map{|line| "Et #{line}!"}.reverse
   end
 
   def self.verse(verse_num)
+    lines = lines_for_verse(verse_num)
+    verse = ["Je te plumerai #{@lines[verse_num]}."]
+    verse += lines
+    return verse.map{ |line| "#{line}\n#{line}" }.join("\n") + @end
   end
 
   def self.sing
+    song = @refrain
+    @lines.count.times do |i|
+      song += "\n\n" + verse(i) + "\n\n" + @refrain
+    end
+    return song
   end
 end
